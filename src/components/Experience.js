3b3f7ac89fdbd29018299d9edb3ef83e82710e8c
enricosebastian <enricosebastiansalazar@gmail.com>
2023-01-23 10:29:51 +0800

    Created module files that have data to be easily editable

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,22 +1,20 @@
 import React from "react";
 import SectionItem from "./SectionItem";
+import { experiences } from "../data/experiences";
 
 export default function Experience() {
+    const sectionItems = experiences.map(experience => 
+        <SectionItem 
+            mainHeading = {experience.mainHeading}
+            duration = {experience.duration}
+            secondaryHeading = {experience.secondaryHeading}
+            descriptionItems = {experience.descriptionItems}
+        />);
+
     return (
         <div className="section" id="experience">
             <h1>experience</h1>
-            <SectionItem 
-                mainHeading={"the lasallian"} 
-                duration={"10/2022 - Present"}
-                secondaryHeading={"front-end developer"}
-                descriptionItems={["help","im","a fish"]}
-            />
-            <SectionItem 
-                mainHeading={"willis towers watson"} 
-                duration={"04/2022 - 06/2022"}
-                secondaryHeading={"full stack developer intern"}
-                descriptionItems={["quick","brown","fox"]}
-            />
+            {sectionItems}
         </div>
     );
 
---
+++ src/data/experiences.js
@@ -0,0 +1,21 @@
+export const experiences = [{
+    mainHeading: "the lasallian",
+    duration: "10/2022 - Present",
+    secondaryHeading: "front-end developer",
+    descriptionItems: [
+        "Created reusable components placed through the website's home page via React.js", 
+        "Integrated the WordPress API to create dynamic components from user-generated posts via Hooks", 
+        "Ensured website design resizing for different web browser sizes and devices"
+    ]
+},{
+    mainHeading: "willis towers watson",
+    duration: "04/2022 - 06/2022",
+    secondaryHeading: "full stack developer",
+    descriptionItems: [
+        "Minimized backend processing delay by almost 200% through refactored server-side queries", 
+        "Spearheaded the implementation of a repository-service design pattern for product’s backend", 
+        "Designed generic interfaces that communicated with controllers & services",
+        "Integrated ability to store and retrieve image files in SQL Server though byte conversion",
+        "Performed SQL Server database modifications and migrations during TFS build deployments"
+    ]
+},];
