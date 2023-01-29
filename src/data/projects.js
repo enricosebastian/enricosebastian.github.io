3c6d907584cbac14c222d365b7ebb4ac82e344ae
enricosebastian <enricosebastiansalazar@gmail.com>
2023-01-29 09:29:39 +0800

    added onhover features for links

--- src/data/projects.js
+++ src/data/projects.js
@@ -12,4 +12,11 @@ export const projects = [{
         "main backend developer that set up image uploading, account creation, and attendance tracking via firebase", 
         "handled user connectivity to the database using sharedpreference implementation"
     ]
-},];
+},{
+    mainHeading: "autonomous trash collecting drone system",
+    secondaryHeading: "tensorflow lite, arduino, raspberry pi, python",
+    descriptionItems: [
+        "trained thousands of images of plastic bottles using tensorflow lite and model maker", 
+        "implemented wireless acknowledgement system between drones and base station"
+    ]
+}];
