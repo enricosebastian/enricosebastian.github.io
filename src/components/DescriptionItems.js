ae9a143416ac020441f739a50a6648bdf667b412
Enrico Sebastian <74126864+enricosebastian@users.noreply.github.com>
2024-02-25 21:50:32 +0800

    Delete src directory

--- src/components/DescriptionItems.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ResponsibilitiesItems(props) {
-    return props.descriptionItems.map(descriptionItem => <li key={descriptionItem}>-{descriptionItem}</li>);
-}
--- src/data/education.js
+++
@@ -1,9 +0,0 @@
-export const education = [{
-    mainHeading: "de la salle university - manila",
-    duration: "expected 2023",
-    secondaryHeading: "electronics engineer, minor in software",
-    descriptionItems: [
-        "cgpa: 3.03/4.00", 
-        "courses: software, mobile, intelligent systems"
-    ]
-},];
