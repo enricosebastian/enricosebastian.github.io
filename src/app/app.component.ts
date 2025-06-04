ecbcb052e61bccb8560a3622c1d95ad7c913b05a
enricosalazaratmb <enrico.salazar@anytimemailbox.com>
2025-06-04 23:48:51 +0800

    Initialized a few components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainWindowComponent } from './components/main-window/main-window.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainWindowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/main-window/main-window.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-window',
+  imports: [],
+  templateUrl: './main-window.component.html',
+  styleUrl: './main-window.component.scss'
+})
+export class MainWindowComponent {
+
+}
