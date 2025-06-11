4123a0b08f40dcf2303ff0f49909bfd74af80307
Enrico Sebastian Salazar <enrico.salazar@anytimemailbox.com>
2025-06-12 00:56:43 +0800

    Added woring iamge container object with window styling

--- src/app/components/image-container/image-container.component.ts
+++ src/app/components/image-container/image-container.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { WindowButtonComponent } from '../window-button/window-button.component';
 
 @Component({
   selector: 'app-image-container',
-  imports: [CommonModule],
+  imports: [CommonModule, WindowButtonComponent],
   templateUrl: './image-container.component.html',
   styleUrl: './image-container.component.scss'
 })
