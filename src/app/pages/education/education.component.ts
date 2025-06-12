ab2f5940bf224532a2e8c9080b1d2669617b2ae4
enricosebastian <4126864+enricosebastian@users.noreply.github.com>
2025-06-12 19:22:56 +0800

    Formatting education page

--- src/app/pages/education/education.component.ts
+++ src/app/pages/education/education.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { PublicPageBase } from '../base/public-page.base';
 import { Meta, Title } from '@angular/platform-browser';
+import { HeaderComponent } from '../../components/header/header.component';
+import { SpanComponent } from '../../components/span/span.component';
+import { ImageContainerComponent } from '../../components/image-container/image-container.component';
+import { ImageSize } from '../../enums/sizes.enums';
 
 @Component({
   selector: 'app-education',
-  imports: [],
+  imports: [HeaderComponent, SpanComponent, ImageContainerComponent],
   templateUrl: './education.component.html',
   styleUrl: './education.component.scss'
 })
 export class EducationComponent extends PublicPageBase {
+  ImageSize = ImageSize;
 
   constructor(meta: Meta, title: Title) {
     super(meta, title);
